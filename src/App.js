ae3e32cb6200743b0c40edaaf4be3419a9d58299
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2024-10-21 01:34:22 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>TV APPS</h1>
+        <div className="app-icons">
+          <Link to="/app/netflix"><img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg
+" alt="Netflix" /></Link>
+          <Link to="/app/hbomax"><img src="https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg
+" alt="HBO Max" /></Link>
+          <Link to="/app/hulu"><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg" alt="Hulu" /></Link>
+          <Link to="/app/primevideo"><img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png" alt="Prime Video" /></Link>
+        </div>
+
+        <Switch>
+          <Route path="/app/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Child() {
+  // Use useParams to get the app id from the URL
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>You Selected: <span>{id}</span></h3>
+    </div>
+  );
+}
